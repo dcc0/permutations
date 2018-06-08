bcb9943a9437e4aa7e3aaa45d066db25548139ed
dcc0 <dcc0@yandex.ru>
2018-06-08 16:11:09 +0300

    Update permutations_with_repetitions.c

--- .gitignore/permutations_with_repetitions.c
+++ .gitignore/permutations_with_repetitions.c
@@ -16,12 +16,12 @@ int main (int argc, char *argv[]) {
         printf("Enter an argument. Example 12233 or 1234:\n");
         return 0;
     }
+	
 /*It calculates an array's length. Вычислим длину строки*/
-
     for (x; argv[1][x]; x++);
 
+	
 /*Buble sort an array. Упорядочим алфавит*/
-
     for(i=0; i < x; i++) {
         for(j = x-1; j > i; j-- ) {
             if (argv[1][j-1] > argv[1][j]) {
@@ -32,25 +32,28 @@ int main (int argc, char *argv[]) {
         }
     }
 
+	
       /*Копируем аргумент в переменную ch
        Copy argument to ch variable*/
       strcpy (ch, argv[1]);
 
       i = x;
       j = 0;
+	
 	/*Перевернем переменную ch - нужна для остановки алгоритма.
 	 Reverse ch. It's needed to stop the algorithm*/
-		while( i > j ) {
+	while( i > j )
+	{
          i--;
          c=ch[j];
          ch[j]=ch[i];
          ch[i]=c;
          j++;
-        }
+        
+	}
 
 
 /*Main part: here we permute. Порождаем перестановки*/
-
     y=0;
 
     while (1) {
@@ -58,6 +61,7 @@ int main (int argc, char *argv[]) {
         printf("%s\n", argv[1]);
 		if (strcmp (argv[1], ch) == 0)  break;
         i=x-2;
+	    
         /*Here we search next. Ищем новую перестановку*/
         while(argv[1][i] >= argv[1][i+1] ) i--;
         j=x-1;
@@ -68,6 +72,7 @@ int main (int argc, char *argv[]) {
         argv[1][j]=argv[1][i];
         argv[1][i]=c;
         i++;
+	    
         /*Tail reverse. Оборачиваем хвост*/
         for (j = x-1; j > i; i++, j--) {
             c = argv[1][i];
